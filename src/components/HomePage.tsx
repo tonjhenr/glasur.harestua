3172cb2d6b505d31d94cdc0ed833b9b31a57854c
Tonje Henriksen <tonjehen@gmail.com>
2026-02-07 01:33:54 +0100

    Endret design på forsiden, footer og koblet login til db,nytt forsøk

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -10,7 +10,7 @@ import { Button } from "./ui/button";
 import focacciaImg from "../assets/img/focaccia.jpg";
 import konfektImg from "../assets/img/konfekt.jpg";
 import wienerbrodImg from "../assets/img/WIENERBRODSNURRER.jpg";
-import hamburgerImg from "../assets/img/hamburgerbrod.jpg";
+import hamburgerImg from "../assets/img/Hamburgerbrod.jpg";
 
 
 type HomePageProps = {
